a8acc4ac0ca9acc25a7307f81988b1672f72db1f
Netanel Bondat <netanelbondar74@gmail.com>
2021-10-19 10:55:01 +0300

    build succesfull

--- include/Hourglass.h
+++ include/Hourglass.h
@@ -1,7 +1,12 @@
 #include "Board.h"
+#include "Triangle.h"
 
 
 class Hourglass {
 public:
 	Hourglass(const Triangle& upper, const Triangle& lower);
+	Hourglass(const Triangle& lower);
+
+	double getLength() const;
+	double getHeight() const;
 };
--- include/Square.h
+++ include/Square.h
@@ -1,4 +1,4 @@
-#include <Board.h>
+#include "Board.h"
 
 class Square {
 public:
