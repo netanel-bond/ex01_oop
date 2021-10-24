1b2183b88a2880e168ee840c31a1710129cd52be
bar oren <bar.oren10@gmail.com>
2021-10-24 22:11:09 +0300

    works with mac

---
+++ cmake-build-debug/CMakeFiles/ex01.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex01.dir/src/Board.cpp.o"
+  "CMakeFiles/ex01.dir/src/Rectangle.cpp.o"
+  "CMakeFiles/ex01.dir/src/Square.cpp.o"
+  "CMakeFiles/ex01.dir/src/Utilities.cpp.o"
+  "CMakeFiles/ex01.dir/src/Vertex.cpp.o"
+  "CMakeFiles/ex01.dir/src/main.cpp.o"
+  "ex01"
+  "ex01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
